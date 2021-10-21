1b2e6348726aa6cf3126fe93ef2853e3270bf2b9
Spencer <Spaopaopao@gmail.com>
2021-10-20 23:20:33 -0400

    Integrate cleaning data to main.py. Can record only jumps/ducks if need

--- Dinosaur Game/Window_capture/main.py
+++ Dinosaur Game/Window_capture/main.py
@@ -6,7 +6,8 @@ import numpy as np
 import os
 from time import time, sleep
 from utils.windowcapture import WindowCapture
-
+from typing import List
+import pandas as pd
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 file_name = "Data/screenshots.npy"
@@ -31,6 +32,23 @@ def save_data(image_data, targets):
     """Function obtained from ClarityCoders: https://github.com/ClarityCoders/Fall-Guys-AI/blob/master/CreateData.py"""
     np.save(file_name, image_data)
     np.save(file_name2, targets)
+
+
+def npy_2_greyscale(data: np.ndarray) -> List[int]:
+    """Gray scales screen shot images from BGR->Gray"""
+    gray = []
+    for img in data:
+        gray.append(cv.cvtColor(img, cv.COLOR_BGR2GRAY))
+    return gray
+
+
+def canny_images(data: np.ndarray) -> List[int]:
+    """Gray scales screen shot images from BGR->Gray
+    data: Gray scaled images."""
+    canny_arr = []
+    for img in data:
+        canny_arr.append(cv.Canny(img, threshold1=100, threshold2=200))
+    return canny_arr
 
 
 def main():
@@ -89,12 +107,33 @@ def main():
             to_remove += 1
         elif ta and t != v:
             break
-
+    '''Removing the error triggers and residual screenshots.'''
     image_data = image_data[:-to_remove]
     target = target[:-to_remove]
-    # Save data.
-    save_data(image_data, target)
-    print("Finished Saving image data and target values.")
+    '''Comment If else statement below if you want to capture no action screenshots'''
+    # Capturing elements that will be thrown out (in this case -1)
+    # IF you don't want non - actions to be captured.
+    if len(target) > 0:
+        res_list = [i for i, value in enumerate(target) if value == -1]
+        print("Grey Scaling...")
+        gray_images = npy_2_greyscale(image_data)
+        print("Canny Edge Detection...")
+        c_imgs = np.asarray(canny_images(gray_images))
+        images_flat = pd.DataFrame(c_imgs[:, :, :].flatten().reshape(c_imgs.shape[0], 417600))
+        # flatten images then converted to dataframe for easier removal of idx
+        images_flat = images_flat.drop(images_flat.index[res_list])
+        target = np.delete(target, res_list)
+        # Save data.
+        print(len(target), images_flat.shape)
+        print(np.unique(target, return_counts=True))
+        save_data(image_data, target)
+        print("Finished Saving image data and target values.")
+    else:
+        print("No Data to save...")
+    # if len(target) > 0:
+    #   save_data(image_data, target)
+    # else:
+    #   print("No Data to save...")
 
 
 if __name__ == '__main__':
