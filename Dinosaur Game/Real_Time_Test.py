0b00900024fd2d6a25a7279a21aec5b335f5f9b3
Spencer <Spaopaopao@gmail.com>
2021-11-11 10:55:15 -0500

    Updating Real time test and requirements with calibration and packages

--- Dinosaur Game/Real_Time_Test.py
+++ Dinosaur Game/Real_Time_Test.py
@@ -6,11 +6,20 @@ import pickle
 import keyboard
 import time
 import xgboost as xgb
-
+import joblib
+import numpy as np
 """CHANGE FILE PATH"""
 # model = pickle.load(open("Modeling/Existing_Models/log-reg.pkl", 'rb'))  # horrible.
-# model = pickle.load(open("Modeling/Existing_Models/xgboost_dino_2.pkl", 'rb'))  # horrible.
-model = pickle.load(open("Modeling/Existing_Models/xgboost_dino_SMOTE.pkl", 'rb'))  # horrible.
+# model = pickle.load(open("Modeling/Existing_Models/xgboost_dino_tuned_2.pkl", 'rb'))
+# model = pickle.load(open("Modeling/Existing_Models/xgboost_dino_tuned.pkl", 'rb'))  # horrible.
+# model = pickle.load(open("Modeling/Existing_Models/xgboost_dino_SMOTE.pkl", 'rb'))  # horrible.
+# horrible.
+# model = pickle.load(open("Modeling/Existing_Models/xgboost_dino_CLOUD_single.pkl", 'rb'))
+# model = joblib.load(open("Modeling/Existing_Models/xgb_gpyopt_3016117.joblib", 'rb'))
+# model = joblib.load(open("Modeling/Existing_Models/xgb_retry.pkl", 'rb'))
+# model = joblib.load(open("Modeling/Existing_Models/xgb_retry_03.pkl", 'rb'))
+model = joblib.load(open("Modeling/Existing_Models/xgb_best.joblib", 'rb'))
+
 model.set_param({'nthread': 2})
 print("Model Loaded.")
 
@@ -27,22 +36,25 @@ def main():
     while(True):
         # get an updated image of the game
         screenshot = wincap.get_screenshot()
+        # time.sleep(0.05)
         # Crop image
-        screenshot = screenshot[0:screenshot.shape[0], 250:700, :]
+        screenshot = screenshot[0:screenshot.shape[0], 330:555, :]  # 250:700 , 375:600
         gray_images = cv.cvtColor(screenshot, cv.COLOR_BGR2GRAY)
         screenshot = cv.Canny(gray_images, threshold1=100, threshold2=200)
-        # 417600 for full view (comment line 121) 129600
-        screenshot = xgb.DMatrix(screenshot.flatten().reshape(1, 129600))
+        screenshot = xgb.DMatrix(screenshot.flatten().reshape(1, 64800))
         start_time = time.time()
         # result = model.predict(screenshot.flatten().reshape(1, 129600))
-        result = model.predict(screenshot)
+        result = np.round(model.predict(screenshot))
         print("Prediction time --- %s seconds ---" %
               (time.time() - start_time), "result: ", result)
         # flatten images then converted to dataframe for easier removal of idx
         # print("Prediction is: ", result)
-        # https://stackabuse.com/guide-to-pythons-keyboard-module/
+        # # https://stackabuse.com/guide-to-pythons-keyboard-module/
         if result[0] == 1:  # Jump!
-            keyboard.send('up')
+            # keyboard.send('up')
+            keyboard.press('up')
+            time.sleep(0.07)
+            keyboard.release('up')
         elif result[0] == 2:  # duck!
             keyboard.send('down')
         else:
